af50394386778fb2e78aae30dfaad6c3fd9baf56
gen-cy <gency@tuta.io>
2022-07-05 17:48:10 -0700

    Updates

---
+++ precache-manifest.cf02f57dec6729789803ba77fdc19a69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e923e0efa296899af18d",
+    "url": "/cedricexposure/static/css/main.c8affb89.chunk.css"
+  },
+  {
+    "revision": "e923e0efa296899af18d",
+    "url": "/cedricexposure/static/js/main.e923e0ef.chunk.js"
+  },
+  {
+    "revision": "c9ff145f0793297b6f50",
+    "url": "/cedricexposure/static/js/1.c9ff145f.chunk.js"
+  },
+  {
+    "revision": "60337411fcd6ce536e2a",
+    "url": "/cedricexposure/static/js/runtime~main.60337411.js"
+  },
+  {
+    "revision": "6a6e680e29ee3e8d93bf70dbcd357345",
+    "url": "/cedricexposure/index.html"
+  }
+];
